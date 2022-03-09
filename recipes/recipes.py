e811ce2e2dd2a0eea90c653a395f3acad8f5f6de
Benjamin Schnitzler <benjamin.schnitzler+git@googlemail.com>
2022-03-09 12:56:34 +0100

    added SimpleForm class and produces working code

--- recipes/recipes.py
+++ recipes/recipes.py
@@ -2,6 +2,7 @@
 
 import wx
 import re
+import functools
 
 class MenuWrapper:
     def __init__(self):
@@ -11,8 +12,8 @@ class MenuWrapper:
         self.create()
 
     def append(self, label, helpString='', wId=wx.ID_ANY, kind=wx.ITEM_NORMAL):
-        match = re.search('[a-zA-Z&]+', label)
-        key = match[0].replace('&', '') if match[0] != None else None
+        match = re.search('[a-zA-Z& ]+', label)
+        key = re.sub('[& ]', '', match[0]) if match[0] != None else None
         item = self.menu.Append(wId, label, helpString, kind)
         if key: self.items['on'+key] = item
 
@@ -31,12 +32,17 @@ class FileMenuWrapper(MenuWrapper):
         self.append(wId=wx.ID_EXIT,
                     label="E&xit",
                     helpString=" Exit the Program")
+        self.append(label="&Import Recipes",
+                    helpString=" Load new recipes into the database")
 
     def onAbout(self, event):
         print('onAbout')
 
     def onExit(self, event):
         print('onExit')
+
+    def onImportRecipes(self, event):
+        print('onImportRecipes')
 
 
 class MainMenuBarWrapper:
@@ -45,17 +51,127 @@ class MainMenuBarWrapper:
         bar.Append(FileMenuWrapper().menu, "&File")
         frame.SetMenuBar(bar)
 
-class MainWindowWrapper:
-    def __init__(self, app, title='', parent=None):
-        frame = wx.Frame(parent, title=title)
-        frame.CreateStatusBar()
-        MainMenuBarWrapper(app, frame)
-        frame.Show()
+class PaddedBox(wx.BoxSizer):
+    def __init__(self,
+                 child,
+                 orient=wx.HORIZONTAL,
+                 proportion=2,
+                 flag=wx.ALL|wx.EXPAND,
+                 border=15):
+        super().__init__(orient)
+        self.Add(child, proportion=proportion, flag=flag, border=border)
 
-class Recipes(wx.App):
-    def OnInit(self):
-        MainWindowWrapper(self, title='Recipes')
-        return True
+class SimpleForm:
+    def __init__(self, parent):
+        self.parent = parent
+        self.rows = []
 
-r = Recipes()
-r.MainLoop()
+    def add_input(self, label = ''):
+        label = wx.StaticText(self.parent, label=label)
+        input = wx.TextCtrl(self.parent)
+        self.rows.append([
+            (label, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL),
+            (input, 1, wx.EXPAND)
+        ])
+
+    def create_grid(self):
+        fgs = wx.FlexGridSizer(
+            rows = len(self.rows),
+            cols = 2,
+            vgap = 10,
+            hgap = 10
+        )
+        flatten_rows = lambda flattened, row: flattened.extend(row), []
+        flattened_ros = functools.reduce(flatten_rows, self.rows)
+        fgs.AddMany(flattened_ros)
+        fgs.AddGrowableCol(1, 1)
+        return fgs
+
+    def create_box(self):
+        return PaddedBox(self.create_grid())
+
+class RecipeTitleForm:
+    def __init__(self, parent, title, meta = {}):
+        form = SimpleForm(parent)
+        form.add_input(label = 'Title')
+
+        # label and input for all other meta fields
+        for key in meta:
+            form.add_input(meta[key]['label'])
+
+        parent.SetSizer(form.create_box())
+
+
+
+class RecipeForm:
+    def __init__(self, parent):
+        RecipeTitleForm(wx.Panel(parent), 'Hello Worlds!')
+
+    def old(self):
+        panel = wx.Panel(parent)
+        fgs = wx.FlexGridSizer(3, 2, 10,10)
+
+        title = wx.StaticText(panel, label = "Title")
+        author = wx.StaticText(panel, label = "Name of the Author")
+        review = wx.StaticText(panel, label = "Review")
+
+        tc1 = wx.TextCtrl(panel)
+        tc2 = wx.TextCtrl(panel)
+        tc3 = wx.TextCtrl(panel, style = wx.TE_MULTILINE)
+
+        fgs.AddMany([(title), (tc1, 1, wx.EXPAND),
+                     (author), (tc2, 1, wx.EXPAND),
+                     (review, 1, wx.EXPAND), (tc3, 1, wx.EXPAND)])
+        fgs.AddGrowableRow(2, 1)
+        fgs.AddGrowableCol(1, 1)
+
+
+        #panel.SetSizer(PaddedBox(fgs))
+
+#class MainWindowWrapper:
+#    def __init__(self, app, title='', parent=None):
+#        frame = wx.Frame(parent, title=title, size = (300, 250))
+#        #frame.CreateStatusBar()
+#        #MainMenuBarWrapper(app, frame)
+#        RecipeForm(frame)
+#        frame.Show()
+
+class MainWindow(wx.Frame):
+    def __init__(self, title='', parent=None):
+        super().__init__(title=title, parent=parent)
+        RecipeForm(self)
+        #panel = wx.Panel(self)
+
+        #hbox = wx.BoxSizer(wx.HORIZONTAL)
+
+        #fgs = wx.FlexGridSizer(3, 2, 10,10)
+
+        #title = wx.StaticText(panel, label = "Title")
+        #author = wx.StaticText(panel, label = "Name of the Author")
+        #review = wx.StaticText(panel, label = "Review")
+
+        #tc1 = wx.TextCtrl(panel)
+        #tc2 = wx.TextCtrl(panel)
+        #tc3 = wx.TextCtrl(panel, style = wx.TE_MULTILINE)
+
+        #fgs.AddMany([(title), (tc1, 1, wx.EXPAND), (author),
+        #             (tc2, 1, wx.EXPAND), (review, 1, wx.EXPAND), (tc3, 1, wx.EXPAND)])
+        #fgs.AddGrowableRow(2, 1)
+        #fgs.AddGrowableCol(1, 1)
+        #hbox.Add(fgs, proportion = 2, flag = wx.ALL|wx.EXPAND, border = 15)
+        #panel.SetSizer(hbox)
+
+        self.Show()
+
+app = wx.App()
+MainWindow(title='recipes')
+#MainWindow(title='recipes')
+app.MainLoop()
+
+#class Recipes(wx.App):
+#    def OnInit(self):
+#        MainWindowWrapper(self, title='Recipes')
+#        return True
+
+#r = Recipes()
+#r.MainLoop()
