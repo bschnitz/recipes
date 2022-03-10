e79f7a9eb7cea95c982449ac60d1081889c852fb
Benjamin Schnitzler <benjamin.schnitzler+git@googlemail.com>
2022-03-10 09:56:16 +0100

    removed commented code

--- recipes/recipes.py
+++ recipes/recipes.py
@@ -170,53 +170,12 @@ class RecipeForm:
         fgs.AddGrowableRow(2, 1)
         fgs.AddGrowableCol(1, 1)
 
-
-        #panel.SetSizer(PaddedBox(fgs))
-
-#class MainWindowWrapper:
-#    def __init__(self, app, title='', parent=None):
-#        frame = wx.Frame(parent, title=title, size = (300, 250))
-#        #frame.CreateStatusBar()
-#        #MainMenuBarWrapper(app, frame)
-#        RecipeForm(frame)
-#        frame.Show()
-
 class MainWindow(wx.Frame):
     def __init__(self, title='', parent=None):
         super().__init__(title=title, parent=parent)
         RecipeForm(self)
-        #panel = wx.Panel(self)
-
-        #hbox = wx.BoxSizer(wx.HORIZONTAL)
-
-        #fgs = wx.FlexGridSizer(3, 2, 10,10)
-
-        #title = wx.StaticText(panel, label = "Title")
-        #author = wx.StaticText(panel, label = "Name of the Author")
-        #review = wx.StaticText(panel, label = "Review")
-
-        #tc1 = wx.TextCtrl(panel)
-        #tc2 = wx.TextCtrl(panel)
-        #tc3 = wx.TextCtrl(panel, style = wx.TE_MULTILINE)
-
-        #fgs.AddMany([(title), (tc1, 1, wx.EXPAND), (author),
-        #             (tc2, 1, wx.EXPAND), (review, 1, wx.EXPAND), (tc3, 1, wx.EXPAND)])
-        #fgs.AddGrowableRow(2, 1)
-        #fgs.AddGrowableCol(1, 1)
-        #hbox.Add(fgs, proportion = 2, flag = wx.ALL|wx.EXPAND, border = 15)
-        #panel.SetSizer(hbox)
-
         self.Show()
 
 app = wx.App()
 MainWindow(title='recipes')
-#MainWindow(title='recipes')
 app.MainLoop()
-
-#class Recipes(wx.App):
-#    def OnInit(self):
-#        MainWindowWrapper(self, title='Recipes')
-#        return True
-
-#r = Recipes()
-#r.MainLoop()
